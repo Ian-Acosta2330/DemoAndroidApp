a5586b941b5b6fd7eda29a1d457225c6f46bb504
Ian-Acosta2330 <ian2330acosta@gmail.com>
2025-08-24 00:14:03 -0600

    Initial submission

---
+++ src/main/java/com/whatamidoing/myapplication/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.whatamidoing.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.hiButton)
+
+        button.setOnClickListener {
+            Log.v("HelloWorld","Button clicked")
+            Toast.makeText(this,"Hi!",Toast.LENGTH_SHORT).show()
+        }
+
+    } // end of method of on create
+
+}
